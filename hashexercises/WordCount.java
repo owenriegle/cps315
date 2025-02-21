480a715010a855cc9cced67a4d049a954de6638f
Riegles427 <riegleowen@outlook.com>
2025-02-21 12:57:53 -0500

    hash

---
+++ hashexercises/WordCount.java
@@ -0,0 +1,51 @@
+package hashexercises;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("5-para-lorem.txt"));
+
+        HashMap<String, Integer> countedWords = new HashMap<>();
+
+        StringBuilder file = new StringBuilder();
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            file.append(" " + line.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", ""));
+        }
+
+        Scanner scanner = new Scanner(file.toString());
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            int num = wordCount(file.toString(), str);
+            countedWords.put(str, num);
+        }
+
+        System.out.println(countedWords);
+
+        scanner.close();
+        in.close();
+    }
+
+    public static int wordCount(String file, String word) {
+        Scanner scanner = new Scanner(file);
+        int num = 0;
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            if (str.equals(word)) {
+                num++;
+            }
+        }
+
+        scanner.close();
+        return num;
+    }
+}
