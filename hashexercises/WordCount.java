02dd52c0a2ad4473d8040fb46bd3034cdfa7bc3c
Riegles427 <riegleowen@outlook.com>
2025-02-25 17:34:22 -0500

    remove unnecessary import

--- hashexercises/WordCount.java
+++ hashexercises/WordCount.java
@@ -3,7 +3,6 @@ package hashexercises;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
